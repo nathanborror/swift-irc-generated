19a423f5e81096bc6d863ede3c3803c697cb5474
Nathan Borror <nathan@nathanborror.com>
2026-02-18 10:09:14 -0800

    Cleaner

--- Sources/IRC/Types/Channel.swift
+++ Sources/IRC/Types/Channel.swift
@@ -2,15 +2,25 @@ import Foundation
 
 public struct Channel: Identifiable, Sendable {
     public var name: String
-    public var topic: String?
-    public var topicSetBy: String?
-    public var topicSetAt: Date?
+    public var topic: Topic?
     public var modes: Set<Mode>
     public var modeParams: [String: String]  // mode -> parameter (e.g., "l" -> "50" for user limit)
     public var created: Date?
     public var members: [String: Set<Prefix>]  // nick -> prefixes
 
     public var id: String { name }
+
+    public struct Topic: Sendable {
+        public var text: String
+        public var nick: String?
+        public var created: Date?
+
+        public init(text: String, nick: String? = nil, created: Date? = nil) {
+            self.text = text
+            self.nick = nick
+            self.created = created
+        }
+    }
 
     public enum Mode: String, Sendable {
         case inviteOnly = "i"
@@ -31,14 +41,13 @@ public struct Channel: Identifiable, Sendable {
         case voice = "+"
     }
 
-    public init(name: String) {
+    public init(name: String, topic: Topic? = nil, modes: Set<Mode>, modeParams: [String : String],
+                created: Date? = nil, members: [String : Set<Prefix>]) {
         self.name = name
-        self.topic = nil
-        self.topicSetBy = nil
-        self.topicSetAt = nil
-        self.modes = []
-        self.modeParams = [:]
-        self.created = nil
-        self.members = [:]
+        self.topic = topic
+        self.modes = modes
+        self.modeParams = modeParams
+        self.created = created
+        self.members = members
     }
 }
--- Sources/IRC/Types/User.swift
+++ Sources/IRC/Types/User.swift
@@ -3,7 +3,7 @@ import Foundation
 public struct User: Identifiable, Sendable {
     public var nick: String
     public var username: String?
-    public var hostname: String?
+    public var host: String?
     public var realname: String?
     public var account: String?
     public var server: String?
@@ -12,13 +12,14 @@ public struct User: Identifiable, Sendable {
     public var id: String { nick }
     public var isAuthenticated: Bool { account != nil }
 
-    public init(nick: String) {
+    public init(nick: String, username: String? = nil, host: String? = nil, realname: String? = nil,
+                account: String? = nil, server: String? = nil, idleSeconds: Int? = nil) {
         self.nick = nick
-        self.username = nil
-        self.hostname = nil
-        self.realname = nil
-        self.account = nil
-        self.server = nil
-        self.idleSeconds = nil
+        self.username = username
+        self.host = host
+        self.realname = realname
+        self.account = account
+        self.server = server
+        self.idleSeconds = idleSeconds
     }
 }
